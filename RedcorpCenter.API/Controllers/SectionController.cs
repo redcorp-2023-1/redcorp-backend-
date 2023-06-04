2de2e397b22ed6fef2e59dd4f2369ab3b6d584f9
FabrizCastro <fabrizziocastro2612@gmail.com>
2023-06-03 19:26:06 -0500

    Creation of section controller

---
+++ RedcorpCenter.API/Controllers/SectionController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RedcorpCenter.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SectionController : ControllerBase
+    {
+        // GET: api/<SectionController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<SectionController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<SectionController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SectionController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SectionController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
